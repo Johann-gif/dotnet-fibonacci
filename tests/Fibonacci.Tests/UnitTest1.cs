9c8d2f1747cd4848c0ac8c419d6d8c41c56812b8
Johann <johann.de-almeida@lacatholille.fr>
2021-10-11 16:30:45 +0200

    :bug: Correction Tests unitaires CI/CD

--- tests/Fibonacci.Tests/UnitTest1.cs
+++ tests/Fibonacci.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace Fibonacci.Tests
@@ -8,7 +10,14 @@ namespace Fibonacci.Tests
         [Fact]
         public async Task Test1()
         {
-            using var fibonacciDataContext = new FibonacciDataContext();
+            var builder = new DbContextOptionsBuilder<FibonacciDataContext>();
+            var dataBaseName = Guid.NewGuid().ToString();
+            builder.UseInMemoryDatabase(dataBaseName);
+
+            var options = builder.Options;
+            var fibonacciDataContext = new FibonacciDataContext(options);
+            await fibonacciDataContext.Database.EnsureCreatedAsync(); 
+            
             var result = await new Fibo(fibonacciDataContext).ExecuteAsync(new[] {"4"});
             Assert.Single(result);
             Assert.Equal(3, result[0]);
